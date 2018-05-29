bccca57738bfb2443bf46d6206b9b3bd4b45807a
Felix Seip <felixseip01@gmail.com>
2018-05-29 19:34:12 +0200

    Added file for the update function of the match info

---
+++ api/match_updater.py
@@ -0,0 +1,37 @@
+import urllib.request
+import json
+
+groupsArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+teams = []
+stages = []
+
+def update_game_info():
+    with urllib.request.urlopen("https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json") as url:
+        data = json.loads(url.read().decode())
+
+        for team in data['teams']:
+            teams.append(team)
+
+        stages.append(data['groups'])
+        stages.append(data['knockout']['round_16'])
+        stages.append(data['knockout']['round_8'])
+        stages.append(data['knockout']['round_4'])
+        stages.append(data['knockout']['round_2_loser'])
+        stages.append(data['knockout']['round_2'])
+
+        counter = 0
+        for stage in stages:
+            if counter == 0:
+                for groupName in groupsArray:
+                    map_team_id_to_name(stage[groupName]['matches'])
+            else:
+                map_team_id_to_name(stage['matches'])
+            counter = counter + 1
+
+
+def map_team_id_to_name(matches):
+    for match in matches:
+        if (type(match['home_team']) is int) and (type(match['away_team']) is int):
+            if not(match['home_team'] > len(teams)) or not(match['away_team'] > len(teams)):
+                match['home_team'] = teams[match['home_team'] - 1]['name']
+                match['away_team'] = teams[match['away_team'] - 1]['name']
